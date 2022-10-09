95be4636017b1e91f167d7460ab459fa4c8a536e
John Vinh <john.michael.vinh2@gmail.com>
2022-10-08 20:25:29 -0400

    Started A12 and added basic menu bar

--- A12/src/GameController.java
+++ A12/src/GameController.java
@@ -1,2 +1,38 @@
-package PACKAGE_NAME;public class GameController {
+/*
+Name: John Vinh
+Course: CST8221
+Assignment: A12
+Professor: Paulo Sousa
+ */
+
+import javax.swing.*;
+import java.awt.*;
+
+public class GameController extends JFrame {
+    private BorderLayout layout;
+
+    public static void main(String[] args) {
+        GameController main = new GameController();
+    }
+
+    GameController() {
+        super("NumPuz");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        layout = new BorderLayout();
+        setSize(800, 600);
+
+        // Menu
+        JMenuBar menuBar = new JMenuBar();
+        JMenu gameMenu = new JMenu("Game");
+        JMenuItem exitMenuItem = new JMenuItem("Exit");
+        gameMenu.add(exitMenuItem);
+        menuBar.add(gameMenu);
+
+        setJMenuBar(menuBar);
+        setVisible(true);
+    }
+
+    private class Controller {
+
+    }
 }
