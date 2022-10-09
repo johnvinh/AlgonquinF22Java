8e1cc0ace2468eaab60c90bffc6c1300b79a7f36
John Vinh <john.michael.vinh2@gmail.com>
2022-10-08 20:38:37 -0400

    Numbered buttons from 1 to dim*dim-1 are now added

--- A12/src/GameController.java
+++ A12/src/GameController.java
@@ -9,7 +9,10 @@ import javax.swing.*;
 import java.awt.*;
 
 public class GameController extends JFrame {
-    private BorderLayout layout;
+    private final BorderLayout layout;
+    private final GridLayout gridLayout;
+    private final JButton[] gameButtons;
+    private int dim = 0;
 
     public static void main(String[] args) {
         GameController main = new GameController();
@@ -17,9 +20,12 @@ public class GameController extends JFrame {
 
     GameController() {
         super("NumPuz");
+        dim = 3;
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         layout = new BorderLayout();
+        gridLayout = new GridLayout(dim, dim);
         setSize(800, 600);
+        setLayout(layout);
 
         // Menu
         JMenuBar menuBar = new JMenuBar();
@@ -27,8 +33,20 @@ public class GameController extends JFrame {
         JMenuItem exitMenuItem = new JMenuItem("Exit");
         gameMenu.add(exitMenuItem);
         menuBar.add(gameMenu);
-
         setJMenuBar(menuBar);
+
+        // Main game area
+        JPanel mainGamePanel = new JPanel(gridLayout);
+        add(mainGamePanel, BorderLayout.WEST);
+
+        // Game buttons
+        gameButtons = new JButton[dim * dim];
+        // there should be dim*dim - 1 buttons, 1 space is left empty
+        for (int i = 0; i < (dim * dim) - 1; i++) {
+            gameButtons[i] = new JButton(String.format("%s", (i + 1)));
+            mainGamePanel.add(gameButtons[i]);
+        }
+
         setVisible(true);
     }
 
