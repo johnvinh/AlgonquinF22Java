98bbeda5dd0e9c0569cb63ecb7af5403b33e313f
John Vinh <john.michael.vinh2@gmail.com>
2022-10-30 17:54:33 -0400

    Create and display splash screen

--- A22/src/GameView.java
+++ A22/src/GameView.java
@@ -1,7 +1,26 @@
 import javax.swing.*;
+import java.awt.*;
 
 public class GameView {
+    public GameView() {
+        GameSplash splash = new GameSplash();
+    }
+
     private class GameSplash extends JWindow {
-        
+        public GameSplash() {
+            JPanel content = new JPanel(new BorderLayout());
+            content.setBackground(Color.BLACK);
+            setBounds(400, 200, 800, 600);
+            JLabel label = new JLabel(new ImageIcon(getClass().getResource("game.png")));
+            content.add(label, BorderLayout.CENTER);
+            setContentPane(content);
+            setVisible(true);
+            try {
+                Thread.sleep(5000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            dispose();
+        }
     }
 }
