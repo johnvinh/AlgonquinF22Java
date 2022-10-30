d45626a6f303538fb9db4b5ba20afa7816c71b3c
John Vinh <john.michael.vinh2@gmail.com>
2022-10-30 17:19:06 -0400

    Move main method to Game and instantiate all three components

--- A22/src/Game.java
+++ A22/src/Game.java
@@ -1,2 +1,11 @@
 public class Game {
+    /**
+     * Run the game.
+     * @param args  arguments to be sent to the application
+     */
+    public static void main(String[] args) {
+        GameModel gameModel = new GameModel();
+        GameView gameView = new GameView();
+        GameController main = new GameController(gameModel, gameView);
+    }
 }
