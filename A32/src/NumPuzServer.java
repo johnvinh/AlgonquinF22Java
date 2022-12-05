72ab0d12755a3e67c91c54d9b924de7af83b9b10
John Vinh <john.michael.vinh2@gmail.com>
2022-12-04 21:55:50 -0500

    Allow every client to communicate simultaneously

--- A32/src/NumPuzServer.java
+++ A32/src/NumPuzServer.java
@@ -10,17 +10,18 @@ public class NumPuzServer {
     ServerSocket socket;
     Socket client;
     Thread newClientThread;
-    Thread listenForMessageThread;
     NumPuzServerGui gui;
     String clientName;
     HashMap<Integer, ArrayList<Integer>> playerStats = new HashMap<>();
     HashMap<Integer, String> playerNames = new HashMap<>();
     int nextClientId = 1;
+    HashMap<Integer, Socket> clientSockets = new HashMap<>();
 
     public NumPuzServer(int port, NumPuzServerGui gui) throws IOException {
         socket = new ServerSocket(port);
         newClientThread = new Thread(new AcceptClient());
         this.gui = gui;
+        // Start listening for new clients forever
         newClientThread.start();
     }
 
@@ -28,7 +29,9 @@ public class NumPuzServer {
 
         @Override
         public void run() {
+            // Always accept new clients (as long as server is up)
             while (true) {
+                Thread listenForMessageThread;
                 try {
                     client = socket.accept();
                     BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
@@ -41,27 +44,39 @@ public class NumPuzServer {
                     newStats.add(0);
                     playerStats.put(nextClientId, newStats);
                     playerNames.put(nextClientId, clientName);
+                    // Keep track of every client's socket so we can access them later
+                    clientSockets.put(nextClientId, client);
                     // Send the ID to the client
                     PrintWriter out = new PrintWriter(client.getOutputStream(), true);
                     out.println("id:" + nextClientId);
                     gui.getLogTextArea().append("Sent client ID to " + clientName + "\n");
+                    // Create a new thread so that each client can listen simulatenously
+                    listenForMessageThread = new Thread(new ListenForMessage(nextClientId));
+                    // Make sure every user has a unique ID
                     nextClientId++;
                 } catch (IOException e) {
                     throw new RuntimeException(e);
                 }
-                listenForMessageThread = new Thread(new ListenForMessage());
                 listenForMessageThread.start();
             }
         }
     }
 
+    /**
+     * Allows each client to listen to the server simultaneously
+     */
     private class ListenForMessage implements Runnable {
+        private int clientId;
 
+        ListenForMessage(int clientId) {
+            this.clientId = clientId;
+        }
         @Override
         public void run() {
             try {
+                Socket clientSocket = clientSockets.get(this.clientId);
                 System.out.println("Started listening for a message");
-                BufferedReader in = new BufferedReader(new InputStreamReader(client.getInputStream()));
+                BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
                 String message;
                 while ((message = in.readLine()) != null) {
                     System.out.println("Still listening");
@@ -70,9 +85,9 @@ public class NumPuzServer {
                         gui.setConfiguration(config);
                         gui.getLogTextArea().append("Received configuration: " + config + "\n");
                     } else if (message.startsWith("requestConfig")) {
-                        PrintWriter out = new PrintWriter(client.getOutputStream(), true);
+                        PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
                         out.println("config:" + gui.getConfiguration());
-                        gui.getLogTextArea().append("Sent current config to " + clientName + "\n");
+                        gui.getLogTextArea().append("Sent current config to " + playerNames.get(this.clientId) + "\n");
                     } else if (message.startsWith("data:")) {
                         String data = message.split(":")[1];
                         String[] dataSplit = data.split(",");
